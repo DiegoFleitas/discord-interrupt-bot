558fffd79b0fe61f27498a11847bde0d50eb2447
Diego Fleitas <diego.fleitas68@gmail.com>
2022-04-08 19:12:43 -0300

    use text to speech / feedback through specific channel

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const client = new Discord.Client();
 /* Config */
 const TOKEN = ""; // Bot TOKEN here
 const OWNER = 0; // Optional, put your own ID here so that you're the only one who can run bot commands
-const SILENCE_MAX_TIME = 90; // Time in seconds of silence before alerting
+const SILENCE_MAX_TIME = 20; // Time in seconds of silence before alerting
+const CHANNEL_ID = ""; // The channel where to alert
 /* Config */
 
 client.on("ready", () => {
@@ -18,6 +19,7 @@ client.on("ready", () => {
 // This variable will be dynamically updated to the ID of the person you specify with !voicecheck <id>
 // best to leave this alone
 let target = 0;
+var scheduledAlert = null;
 
 function joinChannel(channel, message) {
     try {
@@ -29,13 +31,15 @@ function joinChannel(channel, message) {
                 if (speaking["bitfield"] == 1) {
                     console.log(`${member.username} is speaking`);
                     // stop timer
-                    if (typeof timer !== undefined) clearTimeout(timer);
+                    clearTimeout(scheduledAlert);
                 } else {
                     console.log(`${member.username} stopped speaking ${new Date().toTimeString()}`);
                     // start timer
-                    var timer = setTimeout(() => {
+                    scheduledAlert = setTimeout(() => {
                         console.log(`${member.username} has been silent for ${SILENCE_MAX_TIME} seconds ${new Date().toTimeString()}`);
-                        message.author.send(`${member.username} has been silent for ${SILENCE_MAX_TIME} seconds ${new Date().toTimeString()}`);
+                        const msg = `${member.username} has been silent for ${SILENCE_MAX_TIME} seconds`;
+                        client.channels.cache.get(CHANNEL_ID).send(msg, { tts: true });
+                        scheduledAlert = null;
                     }, SILENCE_MAX_TIME * 1000);
                 }
             });
@@ -54,21 +58,24 @@ client.on("message", message => {
             try { message.delete(); } catch (e) { }; // Delete the message if we have the perms to do so
             if (args[1] == null) {
                 // No ID specified
-                message.author.send("You need to put the ID of the person you're trying to voicecheck after the command (example: !voicecheck 1234567890)");
+                const msg = "You need to put the ID of the person you're trying to voicecheck after the command (example: !voicecheck 1234567890)"
+                client.channels.cache.get(CHANNEL_ID).send(msg);
                 return;
             }
             let targetMember = message.guild.members.cache.get(args[1]); // Get member object from ID
             if (targetMember != null) {
                 // Member exists
                 target = args[1]
-                message.author.send(`I set the target to <@${args[1]}>! If they're already in a VC, I'll auto-join. If not, I'll join the VC right after they do!`);
+                const msg = `I set the target to <@${args[1]}>! If they're already in a VC, I'll auto-join. If not, I'll join the VC right after they do!`
+                client.channels.cache.get(CHANNEL_ID).send(msg);
                 console.log(`Now checking voice: ${targetMember.user.username}#${targetMember.user.discriminator} (ID: ${target})`);
                 if (targetMember.voice.channel != null) {
                     joinChannel(targetMember.voice.channel, message); // Join the target's VC if they're already in one
                 }
             } else {
                 // ID is invalid, at least for the message's guild
-                message.author.send("I couldn't find that user in your server, double check the ID?");
+                const msg = "I couldn't find that user in your server, double check the ID?"
+                client.channels.cache.get(CHANNEL_ID).send(msg);
             }
         }
     } catch (err) {
